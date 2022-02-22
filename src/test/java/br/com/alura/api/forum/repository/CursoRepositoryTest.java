e3ac734678e36ae6e1f59e2891f3887aad3baa61
CasttCK <vini.kronemberger@gmail.com>
2022-02-22 16:08:10 -0300

    Adição de alguns testes automatizados

---
+++ src/test/java/br/com/alura/api/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,37 @@
+package br.com.alura.api.forum.repository;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.api.forum.entity.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository repository;
+
+	@Test
+	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+		String nomeCurso = "Spring Boot";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNotNull(curso);
+		Assert.assertEquals(nomeCurso, curso.getNome());
+	}
+
+	@Test
+	public void naoDeveriaCarregarUmCursoCujoONomeNaoEstejaCadastrado() {
+		String nomeCurso = "JPA";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNull(curso);
+	}
+}
